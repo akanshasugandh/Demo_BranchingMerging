7e5ab8eaff652636ef6ac0a46a5fdef378cc64e5
rames <rames@SHIVALIKA>
2024-07-16 11:42:44 +0530

    A Class is Updated 2nd time-Pallavi

--- Demo_BranchingMerging/src/com/demo1/A.java
+++ Demo_BranchingMerging/src/com/demo1/A.java
@@ -5,6 +5,6 @@ public class A {
 	public static void main(String[] args) {
 		
 		System.out.println("A class");
-		System.out.println("This is Client A By- Pallavi");
+		System.out.println("This is Client A By- Pallavi....");
 	}
 }
